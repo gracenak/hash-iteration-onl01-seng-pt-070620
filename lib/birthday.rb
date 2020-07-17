fe12ebf5728b1fb28af2455039059a621d79a5a4
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 00:46:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,10 +11,12 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_greeting)
-   age 
    birthday_greeting.each do |kids_name, age|
      age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+    elsif
+    
   end
 end
 end
