e2ecdc9b2940823ba7bce1a0308da9ab2937dfff
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 00:49:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,16 +10,15 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_greeting)
-   birthday_greeting.each do |kids_name, age|
+def age_appropriate_birthday(birthday_kids)
+   birthday_kids.each do |kids_name, age|
      age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+     end
     elsif
     puts "You are too old for this."
     end
   end
-end
 
 
 
